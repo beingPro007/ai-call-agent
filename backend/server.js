83daef7187ce26cec38544d8e54c956f8f49bb6e
Gautam Rana <22amtics494@gmail.com>
2025-04-27 10:07:44 +0530

    bug:render port issues

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ app.get('/tts', async (req, reply) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 app.listen({ port: PORT }, (err, address) => {
     if (err) {
         app.log.error(err);
